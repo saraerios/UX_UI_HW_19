29d7e27e141bb6b5020717fc97bbb13d06d02d1b
saraerios <117042572+saraerios@users.noreply.github.com>
2023-04-16 15:12:40 -0400

    add in js file info
    add js file info

--- js/index.js
+++ js/index.js
@@ -1,2 +1,19 @@
 console.log ("Your index.js file is loaded correctly!");
-
+$("#viewPalette1").on("click", function(){
+    $(".fadeMe").fadeToggle("slow");
+});
+$(".NueTravel21").hover(function(){
+    $(this).css("background-color", "#B8B8AB");
+    }, function(){
+    $(this).css("background-color", "white");
+});
+$(".Palette11").hover(function(){
+    $(this).css("background-color", "#B8B8AB");
+    }, function(){
+    $(this).css("background-color", "white");
+});
+$(".USDOE31").hover(function(){
+    $(this).css("background-color", "#B8B8AB");
+    }, function(){
+    $(this).css("background-color", "white");
+});
